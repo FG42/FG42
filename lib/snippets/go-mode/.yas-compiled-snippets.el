a38542f45fc7b0789d4fd72149426db6219431c4
amirrezaask <raskarpour@gmail.com>
2020-02-23 22:47:34 +0330

    go snippet

---
+++ lib/snippets/go-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `go-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'go-mode
+                     '(("err" "if err != nil {\n\n}\n" "err_check" nil nil nil "/home/amirreza/.fg42/lib/snippets/go-mode/err_check" nil nil)))
+
+
+;;; Do not edit! File generated at Sun Feb 23 22:37:27 2020
