b066b0de2cbdf6ccc2b84f06fd27b5613a1b2bb5
amirrezaask <raskarpour@gmail.com>
2020-02-23 17:41:32 +0330

    fix path problem

--- lib/extensions/go/init.el
+++ lib/extensions/go/init.el
@@ -14,8 +14,12 @@
   (local-set-key (kbd "M-*") 'pop-tag-mark))
 
 
+
+(defun go-path () (concat (getenv "HOME") "/go"))
+(defun go-path-binary () (concat (go-path) "/bin"))
 (defun extensions/go-initialize ()
-  """Initialize Golang extension."""
+  """Initialize Golang extension ."""
+   (add-to-list 'exec-path (go-path-binary))
    (add-hook 'go-mode-hook 'fg42-go-hook))
 
 (provide 'extensions/go/init)
