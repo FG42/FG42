02467ed8db0a5e133e6f38d28c40cc06a08af7d2
amirrezaask <raskarpour@gmail.com>
2020-02-23 19:49:12 +0330

    simpler code

--- lib/extensions/editor/init.el
+++ lib/extensions/editor/init.el
@@ -56,10 +56,10 @@
   (setq delete-old-versions t)
 
   ;; Font Configuration -----------------------------------
-  (ability fira-font ()
-           "Sets the default font to Fira Mono"
-           (add-to-list 'default-frame-alist '(font . (get-fg42-font)))
-           (set-face-attribute 'default t :font (get-fg42-font)))
+  (ability font ()
+           "Sets the default font to fg42 font"
+           (add-to-list 'default-frame-alist '(cons 'font fg42-font))
+           (set-face-attribute 'default t :font fg42-font))
   ;; ------------------------------------------------------
 
   (cheatsheet-add :group '--HELP--
