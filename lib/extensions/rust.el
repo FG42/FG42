82dd3057fc3e804d7970320570a2c9ec83724882
amirrezaask <raskarpour@gmail.com>
2020-03-18 14:01:55 +0330

    new rust extension

---
+++ lib/extensions/rust.el
@@ -0,0 +1,16 @@
+;;; RustExtention --- Enable Rust support in FG42
+;;; Commentary:
+;;; Code:
+(require 'fpkg)
+(require 'fg42/extension)
+(require 'extensions/rust/init)
+
+;; dependencies
+(depends-on 'rust-mode)
+
+(extension rust
+           :version 0.0.1
+           :on-initialize extensions/rust-initialize
+           :docs "lib/extensions/rust/readme.org")
+(provide 'extensions/rust)
+;;; rust.el ends here
---
+++ lib/extensions/rust/init.el
@@ -0,0 +1,9 @@
+;;; rust-init --- The entry point for Rust extension
+;;; Commentary:
+;;; Code:
+
+(defun extensions/rust-initialize ()
+  "Initialize Rust extension.")
+
+(provide 'extensions/rust/init)
+;;; init ends here.
