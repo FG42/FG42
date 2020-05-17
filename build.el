a9ca053be68140be8e5849905a691a745a45a0d4
Sameer Rahmani <lxsameer@gnu.org>
2020-05-17 14:35:09 +0100

    Fix the build script to install all the dependencies

--- build.el
+++ build.el
@@ -21,27 +21,31 @@
 ;;
 ;;; Commentary:
 ;;; Code:
-(add-to-list 'load-path (concat (getenv "HOME") ".fg42/lib"))
+(add-to-list 'load-path (concat (getenv "HOME") "/.fg42/lib"))
 
-(defvar bootstrap-version nil
-  "Bootstrap version of straight.  This var is used in straight's installer.")
+(require 'fg42)
+(require 'fpkg)
 
-(defun fpkg-initialize ()
-  "Initilize the straight.e package manager and setup necessary hooks."
-  (let ((bootstrap-file "~/.fg42/.fpkg/straight/repos/straight.el/bootstrap.el")
-        (bootstrap-version 5))
+(activate-extensions 'editor
+                     'development
+                     'web
+                     'editor-theme
+                     'javascript
+                     'ruby
+                     'clojure
+                     'haskell
+                     'php
+                     'common-lisp
+                     'python
+                     'serene
+                     'typescript
+                     'arduino
+                     'java
+                     'racket
+                     'irc
+                     'latex)
 
-    (setq straight-base-dir "~/.fg42/.fpkg/")
-    (if (not (file-exists-p bootstrap-file))
-        (with-current-buffer
-            (url-retrieve-synchronously
-             "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-             'silent 'inhibit-cookies)
-          (goto-char (point-max))
-          (eval-print-last-sexp))
-      (load bootstrap-file nil 'nomessage))))
-
-(fpkg-initialize)
+(fg42-initialize)
 
 (provide 'build)
 ;;; build.el ends here
