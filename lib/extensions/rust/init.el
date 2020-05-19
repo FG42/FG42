d28b1d855f7862e02f188ebe7ddaa204d518ed7c
Sameer Rahmani <lxsameer@gnu.org>
2020-05-19 09:35:52 +0100

    Fix the installation bug by ensuring the /usr/local/bin/ exists

--- lib/extensions/rust/init.el
+++ lib/extensions/rust/init.el
@@ -2,10 +2,14 @@
 ;;; Commentary:
 ;;; Code:
 
+
+;;;###autoload
 (defun extensions/rust-initialize ()
   "Initialize Rust extension."
   (add-hook 'rust-mode-hook #'lsp)
   (add-hook 'rust-mode-hook #'cargo-minor-mode)
   (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+
+
 (provide 'extensions/rust/init)
-;;; init ends here.
+;;; init.el ends here
