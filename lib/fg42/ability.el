339817c1d231d3e43de0b8cbe70cf769c848d1cd
amirrezaask <raskarpour@gmail.com>
2020-08-11 20:58:11 +0430

    Add fg42 abilities state, update defability macro.

--- lib/fg42/ability.el
+++ lib/fg42/ability.el
@@ -5,14 +5,13 @@
 (require 'cl-lib)
 (require 'seq)
 
-(defvar fg42--abilities '()
-  "Internal data structure to store abilities.")
-
-(defvar fg42--activated-abilities '()
+(defvar fg42--abilities-state '()
   "Data structure to hold a list of all abilities activated.")
 
+(defvar fg42--all-abilities '() "Plist of all abilites defined, not necessarily activated.")
+
 (cl-defstruct fg42-ability
-  "Each FG42 ability should implement a copy of this structure."
+  "Each FG42 ability should implement a copy of this structure. Registering an Ability has 2 stages, updating state using *defability* macro and then running state usin a runner."
   name
   doc
   ;; deps is a plist of ability dependencies either packages or other abilities.
@@ -22,8 +21,6 @@
   conflicts
   ;; init function initialize the ability.
   init
-  ;;
-  has-initialized
   )
 
 (defun conflicting? (ability1 ability2)
@@ -44,19 +41,41 @@
               )) ability2-conflicts)
     conflicts))
 
-(defun check-for-conflicts-with-activated (activated ability)
+(defun conflicts-with-activated (activated ability)
   "Check if given ABILITY has any conflicts in ACTIVATED abilites."
   (let* ((all-conflicts '()))
     (mapcar (lambda (active)
               (let*
                   ((this-conflicts (conflicting? active ability)))
                 (when (> (length this-conflicts) 0)
-                  (append this-conflicts all-conflicts)))
+                  (append this-conflicts all-conflicts))
 
-            ) activated)))
+                activated)))))
+
+(defun should-ability-be-activated? (name)
+  "Wether given NAME should be activated. An ability should be activated only if it's not in the state yet, and has no conflicts."
+  ;; check if ability is already in the state.
+  (and (not (member name fg42--abilities-state)) (not (conflicts-with-activated fg42--abilities-state (plist-get fg42--all-abilities name)))))
+
+(defun resolve-abilities (ability)
+  "Resolve given ABILITY ability dependencies. Adds given ABILITY ability deps to fg42--activated-abilites if they are not already there."
+  (mapc (lambda (to-resolve-ability)
+          (when (should-ability-be-activated? to-resolve-ability)
+            (activate-ability to-resolve-ability)))
+        (plist-get (fg42-ability-deps ability) :abilities)))
+
+(defun activate-ability (ability)
+  "Activate given ABILITY."
+  ;; Resolve all deps. Package will get installed in runtime since they are side effect.
+  (resolve-abilities ability)
+  ;; put it in the state to treat that as an active one.
+  (plist-put fg42--abilities-state (fg42-ability-name ability) ability))
+
+
+
 ;; should return a function that has a wrapped lambda that changes the state, outter function checks if ability should be activated, if so runs thet function.
-(defmacro defability (name deps doc conflicts &rest init)
-  "Define an ability with the given NAME, DEPS, DOC, CONFLICTS and INIT.
+(defmacro defability (name deps doc conflicts &rest init-fn)
+  "Define an ability with the given NAME, DEPS, DOC, CONFLICTS and INIT-FN.
 
 *deps* should be a list of abilities with the defined ability dependens
 on them.
@@ -65,46 +84,22 @@ on them.
 
 *body* is a block of code which will run as the ability initializer code."
   (declare (doc-string 3) (indent 2))
-  (if (active-ability? name)
+  (let* ((ability (make-fg42-ability
+                   :name name
+                   :doc doc
+                   :deps deps
+                   :conflicts conflicts
+                   :init init-fn)))
+    (plist-put fg42--all-abilities name ability)
 
-      `(setq ,(intern (concat "fg42--abilities/" (symbol-name name)) (make-fg42-ability
-                                                                      :name name
-                                                                      :doc ,doc
-                                                                      :deps ,deps
-                                                                      :conflicts ,conflicts
-                                                                      :init (lambda ()
-                                                                              ,@init)
-                                                                      :has-initialized false))
-             `(message "Ability is not disabled by user."))))
+    (if (should-ability-be-activated? name)
+        `(progn
+           (activate-ability ,ability))
 
-(defun resolve-pkgs (ab)
-  "Resolve all dependencies."
-  (mapc #'depends-on (plist-get (fg42-ability-deps ab) :pkgs)))
+      `(message "ability %s can't be activated." ,name))))
 
-(defun resolve-abilities (ab)
-  (mapc (lambda (to-resolve-ability)
-          (activate-ability to-resolve-ability)) (plist-get (fg42-ability-deps ab) :abilities)))
-
-(defun activate-ability (ab)
-  "Activate given AB by checking."
-  (unless (fg42-ability-has-initialized ab)
-
-    ;; check if ability has any conflicts.
-    (check-for-conflicts-with-activated fg42--activated-abilities ab)
-
-    ;; check if all deps are ready and if not initialze them.
-    (resolve-pkgs ab)
-    (resolve-abilities ab)
-
-    ;; run initialize function
-    (funcall (fg42-ability-init ab))))
-
-
-(defun register-ability (ability-name)
-  "Register the given ABILITY-NAME as an activity."
-  (add-to-list
-   fg42--abilities
-   '(ability-name . (make-fg42-ability :name ability-name))))
-
+(defun fg42/build (state)
+  "fg42/build build up the editor based on given STATE. It basically do a map on the list of abilities and for each of them resolve package dependencies and runs init fcuntion."
+  )
 (provide 'fg42/ability)
 ;;; ability ends here
