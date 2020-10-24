149d9c6b008a19b574fbf5a23030273c2bfa90c0
Sameer Rahmani <lxsameer@gnu.org>
2020-10-24 19:05:39 +0100

    Add threading macros to the utils module

--- core/fg42/utils.el
+++ core/fg42/utils.el
@@ -129,5 +129,34 @@ or
           (last paths))))
 
 
+(defmacro -> (x &optional form &rest more)
+  "Thread the expr through the forms FORM and rest of form in MORE.
+Insert X as the second item in the first form, making a list of
+it if it is not a list already.  If there are more forms, insert
+the first form as the second item in second form, etc."
+  (declare (debug (form &rest [&or symbolp (sexp &rest form)])))
+  (cond
+   ((null form) x)
+   ((null more) (if (listp form)
+                    `(,(car form) ,x ,@(cdr form))
+                  (list form x)))
+   (:else `(-> (-> ,x ,form) ,@more))))
+
+
+(defmacro ->> (x &optional form &rest more)
+  "Thread the expr through the forms FORM and the rest at MORE.
+Insert X as the last item in the first form, making a list of
+it if it is not a list already.  If there are more forms, insert
+the first form as the
+last item in second form, etc."
+  (declare (debug ->))
+  (cond
+   ((null form) x)
+   ((null more) (if (listp form)
+                    `(,@form ,x)
+                  (list form x)))
+   (:else `(->> (->> ,x ,form) ,@more))))
+
+
 (provide 'fg42/utils)
 ;;; utils.el ends here
