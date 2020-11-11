9ea66275878780bcf74fcbeed26ab260929c1520
Sameer Rahmani <lxsameer@gnu.org>
2020-11-11 12:32:40 +0000

    Remove go-eldoc since it's replaced by lsp

--- lib/extensions/go/init.el
+++ lib/extensions/go/init.el
@@ -5,13 +5,15 @@
 (defun fg42-go-hook ()
   ;; move to action
   "Set's up emacs hooks and turn necessary modes on."
+  (lsp-register-custom-settings
+   '(("gopls.completeUnimported" t t)
+     ("gopls.staticcheck" t t)))
   (lsp)
   (with-ability yas
                 (yas-minor-mode-on))
   (setq-local company-backends '(company-capf company-dabbrev company-dabbrev-code))
   (add-hook 'before-save-hook #'lsp-format-buffer t t)
   (add-hook 'before-save-hook #'lsp-organize-imports t t)
-  (add-hook 'go-mode-hook 'go-eldoc-setup)
   (local-set-key (kbd "M-.") #'godef-jump)
   (local-set-key (kbd "M-*") 'pop-tag-mark))
 
